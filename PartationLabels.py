13f216a76457e276aa7c69bc568e118365e50b04
Anicodeth <105945598+Anicodeth@users.noreply.github.com>
2022-10-14 09:55:54 -0700

    Create PartationLabels.py

---
+++ PartationLabels.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIndex={}
+
+        for i,c in enumerate(s):
+            lastIndex[c]=i
+        res=[]
+        size,end=0,0
+        for i,c in enumerate(s):
+            size+=1
+            end = max(end, lastIndex[c])
+
+            if i ==end:
+                res.append(size)
+                size=0
+        return res
