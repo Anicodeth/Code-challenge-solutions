c9697f87195efa8edfa6a994130c276adfbdf665
Anicodeth <105945598+Anicodeth@users.noreply.github.com>
2023-02-12 10:30:32 -0800

    Create MaxCoins.py

---
+++ MaxCoins.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        n = len(piles)
+        backT = n//3
+        piles.sort()
+        i = n-1
+        my_coins = piles[-2]
+        while i >=0:
+            if i == n-1:
+                i -=2
+                backT -=1
+            elif backT > 0:
+                my_coins += piles[i-1]
+                i -= 2
+                backT -=1
+            else:
+                break
+        return my_coins
