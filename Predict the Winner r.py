e727cb02e28b573ee2f9ae0ff6cb44be0c1949a6
Anicodeth <105945598+Anicodeth@users.noreply.github.com>
2023-03-01 23:21:14 -0800

    Create  Predict the Winner r.py

---
+++ Predict the Winner r.py
@@ -0,0 +1,8 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack, res = [], [-1] * len(nums)
+        for i in list(range(len(nums))) * 2: 
+            while stack and (nums[stack[-1]] < nums[i]): 
+                res[stack.pop()] = nums[i]  
+            stack.append(i) 
+        return res
