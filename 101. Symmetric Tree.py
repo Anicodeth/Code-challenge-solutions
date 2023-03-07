e27c0811b4f1488085a390ed5c6827c3c337a31b
Anicodeth <105945598+Anicodeth@users.noreply.github.com>
2023-03-07 06:14:28 -0800

    Create 101. Symmetric Tree.py

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def inorder(self, root):
+      if root:
+        return self.inorder(root.right) + [root.val] + self.inorder(root.left)
+      else: return []
+      
+    def reverse(self, root):
+      if root:
+        return self.reverse(root.left) + [root.val] + self.reverse(root.right)
+      else: return []
+      
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+      
+      left_tree = self.inorder(root.left)
+      right_tree = self.reverse(root.right)
+
+      if left_tree == right_tree:
+        return True
