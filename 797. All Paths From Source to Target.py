f9a2be58332ef6edc27f72825ebe267cbd2a959a
Anicodeth <105945598+Anicodeth@users.noreply.github.com>
2023-04-10 12:10:14 -0700

    Create 797. All Paths From Source to Target.py

---
+++ 797. All Paths From Source to Target.py
@@ -0,0 +1,18 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+      paths = []
+      def dfs(curr, target):
+
+        if curr[-1] == target:
+          paths.append(curr)
+          return
+
+        for nextval in graph[curr[-1]]:
+          
+          dfs(curr + [nextval], target)
+
+      dfs([0], len(graph) - 1)
+
+      return paths
+
+
