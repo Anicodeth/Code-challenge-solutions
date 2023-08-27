282ca0441d7324ecbf794e85014b95f47e99b34f
Ananya Fekeremariam <13ananya13bb@gmail.com>
2023-08-27 00:32:14 -0700

    Update 1218. Longest Arithmetic Subsequence of Given Difference.py

--- 1218. Longest Arithmetic Subsequence of Given Difference.py
+++ 1218. Longest Arithmetic Subsequence of Given Difference.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
         dp = {}
-
+ 
         for num in arr:
             last = dp.get(num - difference, 0)
             dp[num] = last + 1
