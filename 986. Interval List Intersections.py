061e40b0f54a8468bfcae0b602e2f4281c84b74d
Ananya Fekeremariam <13ananya13bb@gmail.com>
2023-09-30 05:15:03 -0700

    Update 986. Interval List Intersections.py

--- 986. Interval List Intersections.py
+++ 986. Interval List Intersections.py
@@ -1,19 +1,14 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        firstList.sort(key = lambda x: x[0])
-        secondList.sort(key = lambda x: x[0])
-
         
         res = []
         for start1, end1 in firstList:
             for start2, end2 in secondList:
-                if start2 <= end1:
+                if start2 <= end1 and end2 >= start1:
                     newinterval = [max(start1,start2), min(end1, end2)]
-                    if newinterval[1] >= newinterval[0]:
-                        res.append(newinterval)
-
-
-
-
+                    res.append(newinterval)
         return res
+
+
+
 
