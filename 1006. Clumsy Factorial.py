5f60de0810552e108579b45f16ec7dd93709b078
Ananya Fekeremariam <13ananya13bb@gmail.com>
2023-10-02 06:03:58 -0700

    Create 1006. Clumsy Factorial.py

---
+++ 1006. Clumsy Factorial.py
@@ -0,0 +1,16 @@
+class Solution:
+    def clumsy(self, n: int) -> int:
+        cal = []
+
+        op = ['*', '//', '+', '-']
+        i = 0
+        while n:
+            cal.append(str(n))
+            n -= 1
+            s = op[i % 4]
+            cal.append(s)
+            i += 1
+        cal.pop()
+
+        return eval("".join(cal))
+            
