07c534924c66215fae856f8e3ad792852aadbcde
Ananya Fekeremariam <13ananya13bb@gmail.com>
2024-04-07 21:53:41 +0300

    Update 1396. Design Underground System Google.py

--- 1396. Design Underground System Google.py
+++ 1396. Design Underground System Google.py
@@ -2,21 +2,18 @@ class UndergroundSystem:
 
     def __init__(self):
         self.customerHistory = {}
-        self.routes = defaultdict(list)
-        
+        self.routes = defaultdict(list)  
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.customerHistory[id] = (stationName, t)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         startStation, startTime = self.customerHistory[id]
-
-        self.routes[(startStation, stationName)].append(t - startTime)
-        
+        self.routes[(startStation, stationName)].append(t - startTime)   
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         times = self.routes[(startStation, endStation)]
-        return sum(times)/len(times)
+        return sum(times) / len(times)
         
 
 
