dc57063e0f5312b05f47a6557cc7d7e84d60e425
Ananya Fekeremariam <13ananya13bb@gmail.com>
2024-05-13 22:23:23 +0300

    Update 101. Symmetric Tree n.py

--- 101. Symmetric Tree n.py
+++ 101. Symmetric Tree n.py
@@ -1,17 +1,17 @@
 class Solution:
-
     def inorder(self, root):
       if root:
         return self.inorder(root.right) + [root.val] + self.inorder(root.left)
-      else: return []
+      else: 
+        return []
       
     def reverse(self, root):
       if root:
         return self.reverse(root.left) + [root.val] + self.reverse(root.right)
-      else: return []
+      else: 
+        return []
       
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-      
       left_tree = self.inorder(root.left)
       right_tree = self.reverse(root.right)
 
